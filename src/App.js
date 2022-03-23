80cebf02a9089a10dbeebf5bcc2e8e54be8c88e8
JacopoX <godneworld@gmail.com>
2022-03-23 01:04:03 +0100

    Start_and_date_input

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from 'react'
+import Loader from './Components/UI/Loader/Loader';
+import Navbar from './Components/UI/Navbar/Navbar';
 
 function App() {
+
+  const [day, setDay] = useState(new Date())
+  const [data, setData] = useState(null)
+  const [startMag, setStartMag] = useState(0)
+
+  useEffect(() => {
+    fetch(`https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=${formatDateISO(day)}&endtime=${getNextDay(day)}`)
+    .then((response) => response.json())
+    .then((data) => {setData(data.features)})
+  }, [day])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body className="App">
+      { data ? (
+        <>
+          <Navbar day={formatDateISO(day)} count={data.length}/>
+          <main className='container-fluid px-md-5 min-vh-100'>
+            <section className='row my-3 d-flex justify-content-center'>
+              <div className='col-12 col-md-6 d-flex justify-content-center justify-content-md-end align-items-center'>
+                <label className='fw-bold' htmlFor='start'>Start date :</label>
+              </div>
+              <div className='col-12 col-md-6 d-flex justify-content-center justify-content-md-start align-items-center'>
+                <input className='btn btn-outline-light'
+                  type='date'
+                  max={formatDateISO(new Date())}
+                  onChange={ (event) => setDay(new Date(event.target.value))}/>
+              </div>
+            </section>
+          </main>
+        </>
+      ) : (
+        <>
+          <Loader />
+        </>)}
+    </body>
   );
+}
+
+function formatDateISO(date){
+  return date.toISOString().substring(0, 10)
+}
+
+function getNextDay(date){
+  let tomorrow = new Date;
+  tomorrow.setDate(tomorrow.getDate()+1)
+  return formatDateISO(tomorrow)
 }
 
 export default App;
---
+++ src/Components/UI/Navbar/Navbar.js
@@ -0,0 +1,5 @@
+export default function Navbar(props){
+    return (
+        <p>Sono la navbar yeee</p>
+    )
+}
